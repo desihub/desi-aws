11abccdb090aadf5360a05e48895e80362eade90
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-04-23 16:50:22 -0700

    Support remap

--- makefile
+++ makefile
@@ -7,19 +7,24 @@ release=$(root)/edr
 # Subdirectory that we want to upload
 select=$(release)
 
+# Build scanning program
 find: find.cpp
 	g++ find.cpp -std=c++20 -lboost_program_options -o $@
 
+# Find: Scan for filesystem structure in the release
 find.json: find
 	./find $(release) > $@
 
+# Select: Batch upload paths into large directories (>10^12 bytes)
 select.json: select.py find.json
 	python3 select.py $(release) --file-tree find.json --selection $(select) --min-size 1000000000000 -o $@
 
+# Upload: Upload batch directories
 upload: upload.py select.json
 	python3 upload.py $(root) \
 		--bucket s3://desiproto \
 		--selection select.json \
+		--remap '{ "$(release)/spectro/data": "raw-spectro-data", "$(release)/target": "target" }' \
 		--max-dirs 100 \
 		--max-workers 128 \
 		2> upload_errors.txt
--- upload.py
+++ upload.py
@@ -5,6 +5,8 @@ from os import system, path
 from datetime import datetime
 import argparse
 
+## Command-line argument parsing
+
 parser = argparse.ArgumentParser(
     prog='upload.py',
     description=
@@ -12,12 +14,17 @@ parser = argparse.ArgumentParser(
 Uploads stuff
     """
 )
-parser.add_argument('root', help='path to the root directory')
+parser.add_argument('root', help='Path to the root directory')
 parser.add_argument('--bucket', help='URI to S3 bucket (s3://bucket)')
-parser.add_argument('--selection', help='path to the list of selected nodes')
-parser.add_argument('--max-dirs', type=int, help='maximum number of selected nodes to upload')
-parser.add_argument('--max-workers', type=int, help='maximum number of workers to use')
-args = parser.parse_args()
+parser.add_argument('--selection', help='Path to the list of selected nodes')
+parser.add_argument('--remap', help=
+"""JSON key-value pairs specifying how specific directories are to be remapped to different targets.
+
+{ "original_directory_1": "target_directory_1", "original_directory_2": "target_directory_2" } """)
+parser.add_argument('--max-dirs', type=int, help='Maximum number of selected nodes to upload')
+parser.add_argument('--max-workers', type=int, help='Maximum number of workers to use')
+args = parser.parse_args() # parse general args
+args.remap = json.loads(args.remap) # parse the JSON
 
 ### FANCY COLORS
 
@@ -82,25 +89,40 @@ while len(select["queued"]) > 0:
     ## Progress fraction indicator
     header = f"[ {index}/{args.max_dirs} ]"
 
-    ## Absolute and relative path to directory/file
-    abspath = path.abspath(subdir)
-    relpath = path.relpath(abspath, args.root)
+    ## Given absolute path to directory/file, find target path
+
+    abs_path = path.abspath(subdir)
+
+    local_root = args.root
+    target_root = args.bucket
+    for key in args.remap:
+        remap_local_root = path.join(local_root, key)
+        remap_target_root = path.join(target_root, args.remap[key])
+        if abs_path.startswith(remap_local_root):
+            local_root = remap_local_root
+            target_root = remap_target_root
+            break
+
+    rel_path = path.relpath(abs_path, local_root)
+
+    local_path = str(abs_path)
+    target_path = str(path.join(target_root, rel_path))
 
     ## Directory or file? 
     ## They have different upload commands, and directory path names need to terminate with /
-    isdir = path.isdir(abspath)
+    isdir = path.isdir(abs_path)
     if isdir:
-        abspath += "/"
-        relpath += "/"
+        local_path += "/"
+        target_path += "/"
         cmd = "sync"
     else:
         cmd = "cp"
 
     ### ATTEMPT 1: S5CMD
 
-    print(f"{timestamp()} {col.BOLD}{col.OKGREEN}{header}{col.OKCYAN} Syncing \"{relpath}\" with s5cmd... {col.ENDC}")
+    print(f"{timestamp()} {col.BOLD}{col.OKGREEN}{header}{col.OKCYAN} Syncing \"{local_path}\" -> \"{target_path}\" with s5cmd... {col.ENDC}")
 
-    s5cmd = os.system(f"s5cmd --numworkers {args.max_workers} --log error --stat {cmd} {abspath} {args.bucket}/{relpath}")
+    s5cmd = os.system(f"s5cmd --numworkers {args.max_workers} --log error --stat {cmd} {local_path} {target_path}")
     
     ## Remove from queue if success
     if os.waitstatus_to_exitcode(s5cmd) == 0:
@@ -111,12 +133,12 @@ while len(select["queued"]) > 0:
         continue
 
     ## Retry with aws-cli if fail
-    print(f"{timestamp()} {col.BOLD}{col.WARNING}{header}{col.OKCYAN} Failed to sync \"{relpath}\" with s5cmd. Retrying with aws-cli... {col.ENDC}")
-    sys.stderr.write("S5CMD ERROR: " + abspath + "\n")
+    print(f"{timestamp()} {col.BOLD}{col.WARNING}{header}{col.OKCYAN} Failed to sync with s5cmd. Retrying with aws-cli... {col.ENDC}")
+    sys.stderr.write("S5CMD ERROR: " + abs_path + "\n")
 
     ### ATTEMPT 2: AWSCLI
 
-    awscli = os.system(f"aws s3 {cmd} {abspath} {args.bucket}/{relpath}")
+    awscli = os.system(f"aws s3 {cmd} {local_path} {target_path}")
 
     ## Remove from queue if success
     if os.waitstatus_to_exitcode(awscli) == 0:
@@ -127,8 +149,8 @@ while len(select["queued"]) > 0:
         continue
 
     ## If failed again, move to end of queue and append to failed list
-    print(f"{timestamp()} {col.BOLD}{col.FAIL}{header} Failed to sync \"{relpath}\" with awscli! {col.ENDC}")
-    sys.stderr.write("AWSCLI ERROR: " + abspath + "\n")
+    print(f"{timestamp()} {col.BOLD}{col.FAIL}{header} Failed to sync with awscli! {col.ENDC}")
+    sys.stderr.write("AWSCLI ERROR: " + abs_path + "\n")
 
     select["queued"].remove(subdir) 
     select["failed"].append(subdir) 
